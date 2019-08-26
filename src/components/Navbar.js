b7a606f01089d930444c87d421c92e42396d71b0
Conrad <theradistone@gmail.com>
2019-08-25 20:35:28 -0400

    Create Footer & Header

---
+++ src/components/Navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-light bg-dark mb-5">
+        <div className="container">
+          <div className="navbar-header">
+            {/* <Link className="navbar-brand text-white text-lg brand-text" to="">
+              MovieScanner
+            </Link> */}
+          </div>
+          <ul className="navbar-nav ml-auto text-light d-inline-block">
+            <li className="nav-item d-inline-block mr-4">
+              <i class="fas fa-arrows-alt" />
+            </li>
+            <li className="nav-item d-inline-block mr-4">
+              <i className="fab fa-react fa-5x" id="react-logo" />
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
