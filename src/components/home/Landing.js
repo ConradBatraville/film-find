4c0e0bf149136a589cad57af48ae2e4f34a28f3f
Conrad <theradistone@gmail.com>
2019-08-26 12:02:19 -0400

    create MovieCard componenet

--- src/components/home/Landing.js
+++ src/components/home/Landing.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { Component } from "react";
 import SearchForm from "./SearchForm";
+import { connect } from ".react-redux";
+import MoviesContainer from "./MoviesContainer";
 
-export default function Landing() {
-  return (
-    <div className="container">
-      <SearchForm />
-    </div>
-  );
+export class Landing extends Component() {
+  render() {
+    const { loading } = this.props;
+
+    return (
+      <div className="container">
+        <SearchForm />
+        {loading ? <Spinner /> : <MoviesContainer />}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = state => ({
+  loading: state.movies.loading
+});
+
+export default connect(mapStateToProps)(Landing);
---
+++ src/components/home/MovieCard.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+export default class MovieCard extends Component {
+  render() {
+    const movie = this.props;
+    return <div></div>;
+  }
+}
